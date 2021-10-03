52eb026b9c94cde28801ce4d4a0e943bf3b3a3e7
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-10-03 17:01:38 +0300

    refactoring of the code using controllers

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const usersRouter = require("./routes/users.router");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4500;
 
 const app = express();
+app.use(express.json());
 app.use(usersRouter);
 
-app.listen(3500, console.log("Server is running on the port 3500"));
+app.listen(PORT, console.log(`Server is running on the port ${PORT}`));
--- routes/users.router.js
+++ routes/users.router.js
@@ -1,30 +1,15 @@
-const e = require("express");
+const express = require("express");
 const { Router } = require("express");
-const path = require("path");
+const usersController = require("../controllers/users.controller");
 
 const router = Router();
-const usersFilePath = path.normalize(__dirname, +"/../db/users.json");
 
-router.get("/users", async (req, res) => {
-  try {
-    const users = JSON.parse(await fs.readFile(usersFilePath));
-    res.status(200).json(users);
-  } catch (e) {
-    res.status(400).json({ error: e.message });
-  }
-});
+router.get("/users", usersController.getAll);
 
-router.get("/users/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const users = JSON.parse(await fs.readFile(usersFilePath));
+router.get("/users/:id", usersController.getOneById);
 
-    const user = users.filter((user) => (user.id = id));
+router.post("/users", usersController.createOne);
 
-    res.status(200).jsom(user);
-  } catch (e) {
-    res.status(400).json({ error: e.message });
-  }
-});
+router.delete("/users/:id", usersController.deleteOne);
 
 module.exports = router;
